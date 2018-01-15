8a0e6bf6ede3da23a852be0e3350efc47200b939
sbstjn <mail@sbstjn.com>
2018-01-15 21:05:44 +0100

    Wait a second after loading more images

--- source/scripts/main.ts
+++ source/scripts/main.ts
@@ -12,6 +12,7 @@ function checkForLoading() {
   const maxHeight = document.documentElement.scrollHeight
   const curHeight = window.scrollY + window.innerHeight
 
+  let timeToWait = 0
   if (Math.abs(maxHeight - curHeight) <= CONFIG_THRESHOLD) {
     const next = images.slice(position, position + IMAGES_RELOAD)
     const node = document.querySelector('#gallery')
@@ -19,10 +20,12 @@ function checkForLoading() {
     if (next.length > 0 && node) {
       node.innerHTML += next.map(image => `<figure><img src="${image}" /></figure>`).join('')
       position += IMAGES_RELOAD
+
+      timeToWait = 1000
     }
   }
   
-  window.requestAnimationFrame(checkForLoading)
+  setTimeout(() => window.requestAnimationFrame(checkForLoading), timeToWait)
 }
 
 document.body.onload = () => window.requestAnimationFrame(checkForLoading)
